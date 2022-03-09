058224bae823deec44576968eaa45bbb47bb66d8
Amiiiiiiiin <90452342+Amiiiiiiiin@users.noreply.github.com>
2022-03-09 03:31:31 +0100

    Add project files.

---
+++ Controllers/CarController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OblOpgave4.Models;
+using OblOpgave4.Managers;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OblOpgave4.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarController : Controller
+    {
+        private CarManager _manager = new CarManager();
+
+        // GET: api/Cars?modelFilter<value> 
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpGet]
+        public ActionResult<IEnumerable<Car>> Get([FromQuery] int? maxPrice)
+        {
+            IEnumerable<Car> cars = _manager.GetAll(maxPrice);
+
+            if (cars.Count() <= 0)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(cars);
+            }
+
+        }
+
+        // GET api/<CarsController>/5
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpGet("{id}")]
+        public ActionResult Get(int id)
+        {
+            Car car = _manager.GetById(id);
+            if (car == null) return NotFound("No such item, id: " + id);
+            return Ok(car);
+        }
+
+        // POST api/<CarsController>
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [HttpPost]
+        public Car Post([FromBody] Car newCar)
+        {
+            return _manager.AddCar(newCar);
+        }
+
+        // DELETE api/<CarsController>/5
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpDelete("{id}")]
+        public Car Delete(int id)
+        {
+            Car car = _manager.GetById(id);
+            return _manager.Delete(id);
+        }
+    }
+}
---
+++ Managers/CarManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OblOpgave4.Models;
+
+namespace OblOpgave4.Managers
+{
+    public class CarManager
+    {
+        private static int nextID = 1;
+        private static List<Car> data = new List<Car>()
+        {
+            new Car() {Id = nextID++, Model = "Audi TT", Price = 100000, LicensePlate = "ABC123"},
+            new Car() {Id = nextID++, Model = "Audi B4", Price = 200000, LicensePlate = "DEF456" },
+            new Car() {Id = nextID++, Model = "Audi B3", Price = 300000, LicensePlate = "GHI789"},
+            new Car() {Id = nextID++, Model = "Audi B6", Price = 400000, LicensePlate = "JKL123"},
+            new Car() {Id = nextID++, Model = "Audi C52", Price = 2523525, LicensePlate = "6436V5"},
+            new Car() {Id = nextID++, Model = "Audi C35", Price = 2000, LicensePlate = "IOTY12"}
+        };
+
+        public List<Car> GetAll(int? maxPrice)
+        {
+            List<Car> result = new List<Car>(data);
+            if (maxPrice != null)
+            {
+                result = result.FindAll(filterCar => filterCar.Price <= maxPrice);
+            }
+            return result;
+        }
+
+        public Car GetById(int id)
+        {
+            return data.Find(car => car.Id == id);
+        }
+
+        public Car AddCar(Car newCar)
+        {
+            newCar.Id = nextID++;
+            data.Add(newCar);
+            return newCar;
+        }
+
+        public Car Delete(int id)
+        {
+            Car car = data.Find(car => car.Id == id);
+            if (car == null) return null;
+            data.Remove(car);
+            return car;
+        }
+    }
+}
